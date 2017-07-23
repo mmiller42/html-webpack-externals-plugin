0d68b8123e7f19c1847621f1b77c8d70e72d6c96
mmiller <me@mmiller.me>
2017-07-22 18:50:34 -0600

    testing tests!

---
+++ test/HtmlWebpackExternalsPlugin.spec.js
@@ -0,0 +1,8 @@
+import HtmlWebpackExternalsPlugin from '../lib/'
+import assert from 'assert'
+
+describe('HtmlWebpackExternalsPlugin', function() {
+  it('does something', function() {
+    assert.equal(true, false, 'Tests are working!')
+  })
+})
