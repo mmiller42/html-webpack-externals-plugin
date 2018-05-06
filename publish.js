47397c117613ddb0d1ddfd7de3956d37913aa80b
Matt Miller <matt.miller@brightbytes.net>
2018-05-06 17:23:46 -0600

    better publish script with prerelease support

---
+++ publish.js
@@ -0,0 +1,20 @@
+const { execSync } = require('child_process');
+
+const commitMessage = execSync('git log -1 --pretty=%B', {
+  cwd: __dirname,
+  shell: process.env.SHELL
+}).trim();
+
+const match = commitMessage.match(/^([0-9]+\.[0-9]+\.[0-9]+)(?:-(.+))?$/);
+if (match) {
+  const [, version, releaseTag] = match;
+  if (releaseTag) {
+    console.log(`Publishing pre-release version ${commitMessage} with release tag ${releaseTag}`);
+    execSync(`npm publish --tag ${releaseTag}`);
+  } else {
+    console.log(`Publishing release version ${version}`);
+    execSync('npm publish');
+  }
+} else {
+  console.log('No release pushed.');
+}
